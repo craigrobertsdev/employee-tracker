f7f0a664aa972837d49ad8cafa4bdecce21c2491
Craig Roberts <craig.roberts11@outlook.com>
2023-03-05 16:15:19 +1030

    Add and get department queries added

--- db/queries.js
+++ db/queries.js
@@ -1,6 +1,67 @@
-class SqlQueries {
-  constructor();
+const mysql = require("mysql2/promise");
 
-  getManagers = "";
-  
+class MySqlConnection {
+  db;
+  constructor() {}
+
+  async addEmployeeToDatabase(firstName, lastName, roleId, managerId) {
+    await this.db.execute(
+      "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
+      firstName,
+      lastName,
+      roleId,
+      managerId
+    );
+  }
+
+  // returns the first name, last name, role (title), department name, salary and manager name for each employee.
+  async getAllEmployees() {
+    const [rows] = await this.db.execute(
+      "SELECT e1.id, e1.first_name, e1.last_name, r.title, d.name AS department, r.salary, CONCAT(e2.first_name, ' ', e2.last_name) AS manager FROM employee e1 JOIN role r ON r.id = e1.role_id JOIN department d ON d.id = r.department_id LEFT JOIN employee e2 ON e1.manager_id = e2.id ORDER BY e1.id"
+    );
+
+    return rows;
+  }
+
+  async getEmployeeIdByName(firstName, lastName) {
+    const [rows] = await this.db.execute("SELECT id from employee WHERE first_name = ? AND last_name = ?", [firstName, lastName]);
+
+    return rows[0].id;
+  }
+
+  async getManagers() {
+    const [rows] = await this.db.execute("SELECT * FROM employee WHERE id IN (SELECT manager_id FROM employee WHERE manager_id IS NOT NULL)");
+    return rows;
+  }
+
+  async getRoles() {
+    const [rows] = await this.db.execute("SELECT id, title, department_id AS department, salary FROM role");
+
+    return rows;
+  }
+
+  async getDepartments() {
+    const [rows] = await this.db.execute("SELECT * FROM department");
+
+    return rows;
+  }
+
+  async addDepartment(name) {
+    console.log("Adding department named: " + name);
+    console.log(typeof name);
+    const [rows] = await this.db.execute("INSERT INTO department (name) VALUES (?)", [name]);
+    return rows;
+  }
+
+  async initaliseDbConnection() {
+    this.db = await mysql.createConnection({
+      host: "localhost",
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: "organisation_db",
+      namedPlaceholders: true,
+    });
+  }
 }
+
+module.exports = MySqlConnection;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql2/promise");
 const cTable = require("console.table");
 require("dotenv").config();
+const MySqlConnection = require("./db/queries");
+
+const db = new MySqlConnection();
 
 let roles, managers, employees;
-let db;
 
 const options = {
   type: "list",
@@ -34,107 +35,107 @@ function findObj(objArr, val) {
   }
 }
 
-async function getManagerId(managers, managerName) {
-  let managerId;
-  if (managerName === "Nil") {
-    managerId = null;
-  } else {
-    const [rows, columns] = await db.execute("SELECT id from employee WHERE first_name = ? AND last_name = ?", [
-      managerName.split(" ")[0],
-      managerName.split(" ")[1],
-    ]);
-    managerId = rows[0].id;
-  }
-
-  return managerId;
+async function getManagerId(managerName) {
+  managerName === "Nil" ? null : await db.getEmployeeIdByName(managerName.split(" ")[0], managerName.split(" ")[1]);
 }
 // Checks whether or not the list of roles has been updated since last call
 async function getRoles() {
-  const [rows, fields] = await db.execute("SELECT * FROM role");
-  roles = rows;
+  return await db.getRoles();
 }
 
 // Checks whether or not the list of managers has been updated since last call
 async function getManagers() {
-  const [rows, fields] = await db.execute("SELECT * FROM employee WHERE id IN (SELECT manager_id FROM employee WHERE manager_id IS NOT NULL)");
-  managers = rows;
+  return await db.getManagers();
 }
 
 async function addEmployee() {
-  await getRoles();
-  await getManagers();
+  roles = await getRoles();
+  managers = await getManagers();
 
   const roleTitles = roles.map((role) => role.title);
   const managerNames = managers.map((manager) => manager.first_name + " " + manager.last_name);
 
-  await inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "firstName",
-        message: "Enter employee's first name:",
-      },
-      {
-        type: "input",
-        name: "lastName",
-        message: "Enter employee's last name:",
-      },
-      {
-        type: "list",
-        name: "role",
-        message: "Enter employee's role:",
-        choices: roleTitles,
-      },
-      {
-        type: "list",
-        name: "manager",
-        message: "Enter employee's manager:",
-        choices: ["Nil", ...managerNames],
-      },
-    ])
-    .then(async (answers) => {
-      // TODO convert role name to role id for adding to db
-      // TODO convert manager name to role id for adding to db
-      const roleId = findObj(roles, answers.role).id;
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "firstName",
+      message: "Enter employee's first name:",
+    },
+    {
+      type: "input",
+      name: "lastName",
+      message: "Enter employee's last name:",
+    },
+    {
+      type: "list",
+      name: "role",
+      message: "Enter employee's role:",
+      choices: roleTitles,
+    },
+    {
+      type: "list",
+      name: "manager",
+      message: "Enter employee's manager:",
+      choices: ["Nil", ...managerNames],
+    },
+  ]);
 
-      const managerId = await getManagerId(managers, answers.manager);
+  const roleId = findObj(roles, answers.role).id;
 
-      const employee = {
-        firstName: answers.firstName,
-        lastName: answers.lastName,
-        role: roleId,
-        manager: managerId,
-      };
+  const managerId = await getManagerId(answers.manager);
 
-      addEmployeeToDatabase(employee);
-    });
+  const employee = {
+    firstName: answers.firstName,
+    lastName: answers.lastName,
+    role: roleId,
+    manager: managerId,
+  };
+
+  addEmployeeToDatabase(employee);
 }
 
-// TODO - finish query to obtain id, first_name, last_name, role (named title), department, salary and manager
 async function viewAllEmployees() {
-  const [rows, fields] = await db.execute(
-    "SELECT e1.id, e1.first_name, e1.last_name, r.title, d.name AS department, r.salary, CONCAT(e2.first_name, ' ', e2.last_name) AS manager FROM employee e1 JOIN role r ON r.id = e1.role_id JOIN department d ON d.id = r.department_id LEFT JOIN employee e2 ON e1.manager_id = e2.id ORDER BY e1.id"
-  );
-
+  const rows = await db.getAllEmployees();
   console.table(rows);
 }
+
 async function updateEmployeeRole() {}
-async function viewAllRoles() {}
+
+async function viewAllRoles() {
+  roles = await db.getRoles();
+
+  console.table(roles);
+}
+
 async function addRole() {}
-async function viewAllDepartments() {}
-async function addDepartment() {}
+
+async function viewAllDepartments() {
+  const departments = await db.getDepartments();
+
+  console.table(departments);
+}
+
+async function addDepartment() {
+  const departments = await db.getDepartments();
+
+  const answer = await inquirer.prompt({
+    type: "input",
+    name: "departmentName",
+    message: "Enter department name: ",
+  });
+  // check whether the department has already been created
+  departments.forEach((department) => {
+    if (department.name === answer.departmentName) {
+      console.log("Department already exists!");
+      return;
+    }
+  });
+
+  await db.addDepartment(answer.departmentName);
+}
 
 function addEmployeeToDatabase(employee) {
-  db.execute(
-    "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
-    employee.firstName,
-    employee.lastName,
-    employee.role,
-    employee.manager,
-    (err, res) => {
-      err ? console.log(err) : console.log(res);
-    }
-  );
+  db.addEmployeeToDatabase(employee.firstName, employee.lastName, employee.role, employee.manager);
 }
 
 async function askQuestions() {
@@ -170,14 +171,7 @@ async function askQuestions() {
 }
 
 async function init() {
-  db = await mysql.createConnection({
-    host: "localhost",
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: "organisation_db",
-    namedPlaceholders: true,
-  });
-
+  await db.initaliseDbConnection();
   askQuestions();
 }
 
